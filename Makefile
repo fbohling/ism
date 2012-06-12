4206edac05b6e2efba42bd179118053289472ff6
fbohling <felix.bohling@googlemail.com>
2012-06-12 21:39:39 +0200

    Remove start.js

--- Makefile
+++ Makefile
@@ -1,14 +1,8 @@
-JS_FILES = \
-		src/start.js \
-		src/svg.js \
-		src/image.js \
-		src/map.js
-
 all: ism.js
 
-ism.js: $(JS_FILES) Makefile
-	rm -rf $@
-	cat $(JS_FILES) > $@
+ism.js: Makefile
+	echo "var ism = {};" > $@
+	cat src/* >> $@
 
 clean:
 	rm -f ism.js ism.min.js
--- ism.js
+++ ism.js
@@ -3,23 +3,6 @@ var ism = {};
     "use strict";
 
     /*
-     * ism.element provides a shortcut to the otherwise verbose creation
-     * of SVGElements. Example: ism.element("rect") will create an
-     * SVGRect element.
-     *
-     * @param {String} tag
-     * @returns {SVGElement}
-     */
-    ism.svg = function (tag) {
-        return document.createElementNS("http://www.w3.org/2000/svg", tag);
-    };
-
-    return ism;
-}(ism));
-(function (ism) {
-    "use strict";
-
-    /*
      * A map item that contains a plain image. The image can be any format
      * that is supported by the browser, e.g. PNG, JPEG, GIF, TIFF, BMP
      *
@@ -74,6 +57,23 @@ var ism = {};
         };
 
         return map;
+    };
+
+    return ism;
+}(ism));
+(function (ism) {
+    "use strict";
+
+    /*
+     * ism.element provides a shortcut to the otherwise verbose creation
+     * of SVGElements. Example: ism.element("rect") will create an
+     * SVGRect element.
+     *
+     * @param {String} tag
+     * @returns {SVGElement}
+     */
+    ism.svg = function (tag) {
+        return document.createElementNS("http://www.w3.org/2000/svg", tag);
     };
 
     return ism;
