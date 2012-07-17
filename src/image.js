a30e58a2796b7271ab2a3527bb7d24c4571b9d40
fbohling <felix.bohling@googlemail.com>
2012-07-17 22:39:14 +0200

    Correct documentation in src/image.js

--- src/image.js
+++ src/image.js
@@ -3,10 +3,10 @@
 
     /*
      * A map item that contains a plain image. The image can be any format
-     * that is supported by the browser, e.g. PNG, JPEG, GIF, TIFF, BMP
+     * that is supported by the browser, e.g. PNG, JPEG, GIF
      *
      * @constructor
-     * @class - A layer consisting of a plain image
+     * @class - A simple layer consisting of a plain image
      * @param url - The URL of the raster image
      */
     ism.image = function (spec) {
@@ -14,18 +14,20 @@
             width = spec.width || 256,
             height = spec.height || 256,
             url = spec.url,
-            element = ism.svg("image");
+            element = ism.svg("image"),
+            map;
 
         element.setAttribute("width", width);
         element.setAttribute("height", height);
         element.setAttributeNS("http://www.w3.org/1999/xlink",
             "href", url);
 
-        image.map = function (map) {
+        image.map = function (m) {
             if (arguments.length === 0) {
-                return spec.map;
+                return map;
             }
-            map.container().appendChild(element);
+            m.container().appendChild(element);
+            map = m;
             return image;
         };
 
